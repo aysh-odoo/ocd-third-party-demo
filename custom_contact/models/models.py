7fb0f816a819c82c593f6dea572ab2f999c37be0
aysh-odoo <aysh@odoo.com>
2025-07-28 14:54:58 +0530

    Update models.py

--- custom_contact/models/models.py
+++ custom_contact/models/models.py
@@ -4,4 +4,13 @@ from odoo import models, fields, _, api
 class Contact(models.Model):
     _inherit = 'res.partner'
 
-    custom_contact_note = fields.Text('Custom Contact Notes')
+    contact_source = fields.Selection(
+        selection=[('website', 'Website'),
+                   ('referral', 'Referral'),
+                   ('campaign', 'Campaign'),
+                   ('social_media', 'Social Media'),
+                   ('cold_call', 'Cold Call')],
+        string="Contact Source",  
+        help="Track the origin of the contact",
+        default='website'  
+    )
